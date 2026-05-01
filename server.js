ba9d04c7959bcacae06e227169fc7d3d83ed0cde
Keshav Goel <keshavgoel892@gmail.com>
2026-05-01 12:50:27 +0530

    fix server path

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../build/web')));
+app.use(express.static(path.join(__dirname, 'build/web')));
 
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, '../build/web', 'index.html'));});
+  res.sendFile(path.join(__dirname, 'build/web', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
